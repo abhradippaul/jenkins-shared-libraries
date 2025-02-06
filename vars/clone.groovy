3ac35f7aec0e7bc29e9f90c78b6f780f31d8c061
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2025-02-06 23:41:21 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 def(){
   git url: "https://github.com/abhradippaul/Jenkins.git", branch: "main"
-  sh "mkdir /test"
-  sh "cd /test"
+  mkdir /test
+  cd /test
   git clone "https://github.com/abhradippaul/Jenkins.git"
 }
