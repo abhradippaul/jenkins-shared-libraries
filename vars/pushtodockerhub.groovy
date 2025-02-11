e4e5ff1b4f9a1bcaf1e8de1449dcf86686026941
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2025-02-12 00:40:59 +0530

    Update pushtodockerhub.groovy

--- vars/pushtodockerhub.groovy
+++ vars/pushtodockerhub.groovy
@@ -1,8 +1,7 @@
 def call(){
-  withCredentials([usernamePasssword(credentialsId: 'DockerHub', usernameVariable: 'username', passwordVariable: 'password')]){
-    echo "Your username is ${username} and password is ${password}"
-    // sh "docker login -u ${username} -p ${password}"
-    // sh "docker tag backend-image abhradippaul/backend-image"
-    // sh "docker push abhradippaul/backend-image"
+  withCredentials([usernamePassword(credentialsId: 'DockerHub', usernameVariable: 'username', passwordVariable: 'password')]){
+    sh "docker login -u ${username} -p ${password}"
+    sh "docker tag backend-image ${username}/backend-image"
+    sh "docker push ${username}/backend-image"
   }
 }
