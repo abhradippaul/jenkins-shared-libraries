138ddd66c9854cee7bbd4812ddc4bb964fc46df5
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2025-02-12 00:44:41 +0530

    Delete vars/push-to-dockerhub.groovy

--- vars/push-to-dockerhub.groovy
+++
@@ -1,8 +0,0 @@
-def call(){
-  withCredentials([usernamePasssword(credentialsId: "DockerHub", usernameVariable: "username", passwordVariable: "password")]){
-    echo "Your username is ${username} and password is ${password}"
-    // sh "docker login -u ${username} -p ${password}"
-    // sh "docker tag backend-image abhradippaul/backend-image"
-    // sh "docker push abhradippaul/backend-image"
-  }
-}
