672780703539bef56f8961d4d7f398615c498307
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2025-02-12 23:24:35 +0530

    Update testcontainer.groovy

--- vars/testcontainer.groovy
+++ vars/testcontainer.groovy
@@ -1,5 +1,4 @@
 def call(){
-  // sh "url=localhost:3000"
-  sh "url='localhost:3000'; curl ${url} >> log.txt; curl ${url}/api/users >> log.txt; curl ${url}/api/status >> log.txt; curl ${url}/api/products >> log.txt;"
+  sh "curl localhost:3000 >> log.txt; curl localhost:3000/api/users >> log.txt; curl localhost:3000/api/status >> log.txt; curl localhost:3000/api/products >> log.txt;"
   sh "docker stop container1"
 }
